9c637adcdd6c0361caa23c3e8d7e078086a2f312
orkhan-source <orxanbeylerli@gmail.com>
2023-01-20 12:49:38 +0400

    started

--- src/main/java/App/Launcher.java
+++ src/main/java/App/Launcher.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TinderAppApplication {
+public class Launcher {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TinderAppApplication.class, args);
+		SpringApplication.run(Launcher.class, args);
 	}
 
 }
--- src/test/java/App/LauncherTests.java
+++ src/test/java/App/LauncherTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TinderAppApplicationTests {
+class LauncherTests {
 
 	@Test
 	void contextLoads() {
